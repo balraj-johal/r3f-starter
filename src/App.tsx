383749ae7a84e5edcb314c689894dfd1ac1282f0
Balraj Johal <balrajsjohal@gmail.com>
2023-02-19 19:25:21 +0000

    Add treetop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import './App.css'
+import Treetop from './Treetop'
 
 function App() {
   return (
     <div className="App">
       <Canvas>
         <OrbitControls />
+        <Treetop />
       </Canvas>
     </div>
   )
