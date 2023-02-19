fd8d02a5b1eba3e5abc36406b340800cff50aa39
Balraj Johal <balrajsjohal@gmail.com>
2023-02-19 15:21:27 +0000

    Setup fullscreen canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Canvas>
+        <OrbitControls />
+      </Canvas>
     </div>
   )
 }
